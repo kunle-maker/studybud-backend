2616cd5f3af5ec943cb7931309717c9fd25d8993
Kunle the one <kunletheone@gmail.com>
2026-06-05 18:38:18 +0100

    Add files via upload

--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,8 @@
 export const USAGE_LIMITS = {
   FREE: {
-    summaries: 5,
-    teacher:   5,
-    topic:     5,
-    ocr:       5
+    summaries: 3,
+    teacher:   3,
+    topic:     3,
+    ocr:       3
   }
 };
